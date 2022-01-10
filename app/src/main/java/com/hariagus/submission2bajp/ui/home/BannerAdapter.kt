4b265b16c0dee074565c7d5eb2bb7ac2b449eca1
HariAgus <haryagus97@gmail.com>
2022-01-10 13:50:30 +0700

    Update diff util callback

--- app/src/main/java/com/hariagus/submission2bajp/ui/home/BannerAdapter.kt
+++ app/src/main/java/com/hariagus/submission2bajp/ui/home/BannerAdapter.kt
@@ -2,22 +2,23 @@ package com.hariagus.submission2bajp.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import com.hariagus.submission2bajp.R
 import com.hariagus.submission2bajp.data.source.local.entity.TrendingEntity
 import com.hariagus.submission2bajp.databinding.ItemTrendingBinding
+import com.hariagus.submission2bajp.utils.CatalogueDiffUtil
 import com.hariagus.submission2bajp.utils.loadImageGlide
 import com.smarteist.autoimageslider.SliderViewAdapter
 
 class BannerAdapter : SliderViewAdapter<BannerAdapter.ViewHolder>() {
 
-    private var listMovies = ArrayList<TrendingEntity>()
+    private var listBanner = emptyList<TrendingEntity>()
     private val sizeBanner = 5
 
-    fun setMovies(movies: List<TrendingEntity>) {
-        listMovies.apply {
-            clear()
-            addAll(movies)
-        }
+    fun setMovies(banner: List<TrendingEntity>) {
+        val bannerDiffUtil = CatalogueDiffUtil(listBanner, banner)
+        DiffUtil.calculateDiff(bannerDiffUtil)
+        listBanner = banner
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?): ViewHolder {
@@ -33,7 +34,7 @@ class BannerAdapter : SliderViewAdapter<BannerAdapter.ViewHolder>() {
 
 
     override fun onBindViewHolder(viewHolder: ViewHolder?, position: Int) {
-        viewHolder?.bind(listMovies[position])
+        viewHolder?.bind(listBanner[position])
     }
 
     class ViewHolder(private val binding: ItemTrendingBinding) :
